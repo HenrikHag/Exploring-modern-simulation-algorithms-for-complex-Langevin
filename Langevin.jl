17037adac000fcf9c527a8681eb66e8fff4729b7
god245673 <god245673@DESKTOP-NHB0HIV.localdomain>
2022-03-16 20:04:43 +0100

    Langevin coupling, factor of 2 off in beta or m

--- Langevin.jl
+++ Langevin.jl
@@ -5,9 +5,11 @@ gaussianD = Normal(0,1)
 
 #       μ/2*ϕ²+λ/4!*ϕ⁴
 #       -> (μϕ+λ/6*ϕ³)*Δt
-function Action(mu,la,F,dt)
-   return -(mu*F+0*la/6*F^3)*dt
+function ActionDer(a,m,mu,la,F,f₋₁,f₊₁)
+   return m/a*(F-1/2*(f₋₁+f₊₁)) + mu*F + la/6*F^3
 end
+# Add cupling terms 2f(i)-f(i+1)-f(i-i)
+# Understand the discretizing integral and meeting mat. from 16.03
 
 function Langevin(N,mu,la,gaussianD)
     F = [20. for i = 1:16]
@@ -16,22 +18,24 @@ function Langevin(N,mu,la,gaussianD)
     for i=1:N
         push!(Flist,F)
         for ii = 1:length(F)
-            F[ii] += Action(mu,la,F[ii],dt) + sqrt(2*dt)*rand(gaussianD)
+            F[ii] -= Action(mu,la,F[ii],dt) - sqrt(2*dt)*rand(gaussianD)
         end
     end
     return Flist
 end
-function Langevin(N,mu,la,gaussianD,filename)
+function Langevin(N,a,m,mu,la,gaussianD,filename)
     path = "results/"
     F = [20. for i = 1:16]
     Flist = []
     dt = 0.01
     n_burn = 200
+    n_skip = 200
     for i=1:n_burn
         for ii = 1:length(F)
-            F[ii] += Action(mu,la,F[ii],dt) + sqrt(2*dt)*rand(gaussianD)
+            F[ii] -= ActionDer(a,m,mu,la,F[ii],F[(ii-2+n_tau)%n_tau+1],F[(ii)%n_tau+1])*dt - sqrt(2*dt)*rand(gaussianD)
         end
     end
+    show(F);println()
     for i=1:N
         push!(Flist,F)
         open(string(path,filename),"a") do file
@@ -61,20 +65,29 @@ function Langevin(N,mu,la,gaussianD,filename)
             end
             write(file,string(twopointcorr/pathl,"\n"))
         end
-        for ii = 1:length(F)
-            F[ii] += Action(mu,la,F[ii],dt) + sqrt(2*dt)*rand(gaussianD)
+        for iii = 1:n_skip+1
+            for ii = 1:length(F)
+                F[ii] -= ActionDer(a,m,mu,la,F[ii],F[(ii-2+n_tau)%n_tau+1],F[(ii)%n_tau+1])*dt - sqrt(2*dt)*rand(gaussianD)
+            end
         end
     end
     return Flist
 end
-println(Langevin(20,1,0.4,gaussianD,"CL_1.csv"))
-Langv1=Langevin(100000,1,0.4,gaussianD,"CL_1.csv")
+# println(Langevin(20,1,0.4,gaussianD,"CL_1.csv"))
+
+begin
+    n_tau=16
+    β=8
+    a=β/n_tau
+    Langv1=Langevin(15000,a,1,1,0,gaussianD,"CL_1.csv")
+end
+
 # Probability density diagram #
 arr1 = [Langv1[i][j] for i = 1:length(Langv1) for j = 1:length(Langv1[1])]
 histogram(arr1,normed=true,xlabel="x",ylabel="|ψ_0|²")
 histogram(Langv1,normed=true,xlabel="x",ylabel="|ψ_0|²")
 PlotProbDD("results/CL_1.csv",0.1)
-PlotProbDDe(1/2,1,1,3)
+PlotProbDDe(1,1,1,3)
 # sampling
 plot(reshape(GetColumn(2,"results/CL_1.csv"),:))#:Int((length(LastRowFromFile(file))-1)/4)+1
 # Autocorrelation
